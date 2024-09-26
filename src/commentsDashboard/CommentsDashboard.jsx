b1a1f45cb967ea45c82ba5b9e16b49110a97ba6a
Natasza ASzatan <natiye.nb@gmai.com>
2024-09-26 10:45:50 +0200

    created hook to fetch data from given database

--- src/commentsDashboard/CommentsDashboard.jsx
+++ src/commentsDashboard/CommentsDashboard.jsx
@@ -1,8 +1,26 @@
 import styles from "./commentsDashboard.module.css";
 import UserButton from "../userButton/UserButton.jsx";
 import PostsList from "../postsList/PostsList.jsx";
+import { useEffect, useState } from "react";
+
+const useFetchedUsers = () => {
+  const [users, setUsers] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data))
+      .catch((error) => {
+        console.log("error fetching data", error);
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
+  return { users, isLoading };
+};
 
 const CommentsDashboard = () => {
+  const { users, isLoading } = useFetchedUsers();
+  console.log(users);
   return (
     <div>
       <div className={styles.buttonsRow}></div>
