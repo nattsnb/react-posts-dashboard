d0c1bf0d7f8c3381b2b3c111405086796bac54eb
Natasza ASzatan <natiye.nb@gmai.com>
2024-09-27 18:07:40 +0200

    added two buttons at the top of the page. they switch the diplayMode beetwen Posts or Photos.

--- src/commentsDashboard/index.jsx
+++ src/commentsDashboard/index.jsx
@@ -3,6 +3,11 @@ import PostsList from "../postsList/";
 import { useEffect, useState } from "react";
 import { Button, ButtonGroup } from "@mui/material";
 
+const DisplayMode = {
+  posts: "posts",
+  photos: "photos",
+};
+
 const useFetchedUsers = () => {
   const [users, setUsers] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
@@ -21,13 +26,39 @@ const useFetchedUsers = () => {
 const CommentsDashboard = () => {
   const { users, isLoading } = useFetchedUsers();
   const [activeUser, setActiveUser] = useState(null);
+  const [displayMode, setDisplayMode] = useState(DisplayMode.posts);
 
   const newActiveUser = (userId) => {
     setActiveUser(userId);
   };
 
+  const switchDisplayModeToPhotos = () => {
+    setDisplayMode(DisplayMode.photos);
+  };
+
+  const switchDisplayModeToPosts = () => {
+    setDisplayMode(DisplayMode.posts);
+  };
+
   return (
     <>
+      <div>
+        <Button
+          variant={"contained"}
+          color={displayMode === DisplayMode.posts ? "success" : ""}
+          onClick={switchDisplayModeToPosts}
+        >
+          Posts
+        </Button>
+        <Button
+          variant={"contained"}
+          color={displayMode === DisplayMode.photos ? "success" : ""}
+          onClick={switchDisplayModeToPhotos}
+        >
+          Photos
+        </Button>
+      </div>
+
       <h1>Posts List</h1>
       {isLoading && <p>Loading...</p>}
       {!isLoading && (
@@ -44,10 +75,13 @@ const CommentsDashboard = () => {
               </Button>
             ))}
           </ButtonGroup>
-          {activeUser && (
+          {activeUser && displayMode === DisplayMode.posts && (
             <div className={styles.postsList}>
               <PostsList activeUser={activeUser} />
             </div>
+          )}
+          {activeUser && displayMode === DisplayMode.photos && (
+            <div className={styles.postsList}>Photos Placeholder</div>
           )}
         </div>
       )}
